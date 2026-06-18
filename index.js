5086a3e255d448cc4ff503047245387512d7e72f
barack <barackannan00@gmail.com>
2026-06-18 13:53:35 +0300

    Refactor calculateTax function and update module exports

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+function calculateTax(amount) {
+  let tax = amount * 0.1;
+  return tax;
+}
 
-
-
-
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// This is required for the test to function properly
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
