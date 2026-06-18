4f12aa74198f46aff4c90202a1cc27df6a8bc847
barack <barackannan00@gmail.com>
2026-06-18 14:08:59 +0300

    Add findMaximum function and log output in index.js

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ function convertToUpperCase(text) {
   return text.toUpperCase();
 }
 
+function findMaximum(num1, num2) {
+   if (num1 > num2) { return num1}
+   if (num2 > num1) { return num2}
+}
+
+
+console.log(findMaximum(2,3))
 
 // This is required for the test to function properly
 module.exports = {
