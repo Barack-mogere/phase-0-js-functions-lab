a15780de3aac63eb91a1f5b7cfef92fb6fdf0ef0
barack <barackannan00@gmail.com>
2026-06-18 14:32:28 +0300

    Add isPalindrome and calculateDiscountedPrice functions to index.js

--- index.js
+++ index.js
@@ -12,8 +12,16 @@ function findMaximum(num1, num2) {
    if (num2 > num1) { return num2}
 }
 
+function isPalindrome(word) {
+  let reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
+}
 
-console.log(findMaximum(2,3))
+function calculateDiscountedPrice(originalPrice,discountPercentage){
+    let finalPrice = originalPrice * ((100 - discountPercentage)/100)
+     return finalPrice
+}
+
 
 // This is required for the test to function properly
 module.exports = {
